503c6f6ad408fb1eb61e53147184c1dfd6f72d98
Arsen <mr.landos@gmail.com>
2015-08-03 22:32:26 +0300

    Lesson 2 and 3 home work update

---
+++ Lesson2/src/com/company/lesson3/Main.java
@@ -0,0 +1,165 @@
+package com.company.lesson3;
+
+/**
+ * Created by Arsen on 02.08.15.
+ */
+public class Main {
+    public static void main(String[] args) {
+////    int a=20;
+////    int b=a+5;
+////    System.out.println(a==20.3);
+////    String s="0";
+//    int[] array = {1, 2, 3, 4, 5, 5, 5, 1};
+////    System.out.println(array[1] + "    "+ array[4]);
+////    System.out.println(array[1] - array[4]);
+//
+//    for (int i=0; i<array.length; i++)
+//    {
+//        if (array[i]==5) {
+//            System.out.println(i);
+//    }
+//    //System.out.println(a==
+//    String t="Hello Class,";
+//    String ts=" and Student!";
+//    System.out.println(t + ts);
+////Hello Class, and Student!
+//    int[] u={1,2,3,4,5,6,7};
+//    System.out.println(u[5]);
+//// Under key five
+//    System.out.println(u[3-1]);
+//// Under key one but three
+//    for (int x=8; x < 11; x++) {
+//        System.out.print(" " + x);
+//    }
+//    System.out.println(" ");
+//// 8
+//// 9
+//// 10
+//    String[] s={"How","are","you","doing","fucka"};
+//    for (int i = 0; i < 3; i++) {
+//        System.out.println(s[i]);
+//    }
+//// How
+//// are
+//// you
+//
+//    int b=25;
+//    int[] m = {10, 20, 25, 30};
+//    int i = 0;
+//    while (i < 4) {
+//        if (m[i] == b) {
+//            break;
+//        }
+//        System.out.println(m[i]);
+//        i++;
+//    }
+//// 10
+//// 20
+//    System.out.println(" ");
+//
+//
+//
+//
+//    int d=10;
+//    int[] n = {10, 20, 25, 30};
+//    int j = 0;
+//    while (j < 4) {
+//        if (n[j] == d) {
+//            j++;
+//            continue;
+//        }
+//        System.out.println(n[j]);
+//        j++;
+//    }
+//// 20
+//// 25
+//    System.out.println(" ");
+
+//    int g = -2;
+//    int sec = 18;
+//// -36
+//    int g = 0;
+//    int sec = -1000;
+// 0
+//    int g = 113;
+//    int sec = 1;
+//
+//    int r=g*sec;
+//    System.out.println(r);
+//// 113
+//    System.out.println(" ");
+//    boolean oper = true;
+//    int a = 10;
+//    int b = -8;
+//    if (a>3 && b<0);
+//    {
+//        System.out.println(oper);
+//    }
+// 2
+//    boolean oper = false;
+//    int a = 3;
+//    int b = 2;
+//    if (a>3 && b<0);{
+//        System.out.println(oper);
+//    }
+////// 1
+//    boolean oper = false;
+//    int a = 0;
+//    int b = -200;
+//    if (a>3 && b<0);{
+//        System.out.println(oper);
+//    }
+
+//// 200
+//    boolean oper = true;
+//    int a = 999;
+//    int b = 1;
+//    if (oper!=false)
+//    {
+//        System.out.println(a + b);
+//    }
+//     else {
+//    System.out.println(a-b);
+//}
+//    boolean anti = true;
+//    if (anti!=true){
+//        System.out.println("false");}
+//    else {
+//        System.out.println("true");
+//    }
+//// false
+//    boolean anti1 = false;
+//    if (anti1!=true){
+//        System.out.println("false");}
+//    else {
+//        System.out.println("true");}
+//
+//    String[] strings = {"Hello","World","Class", "Guys", "Girls", "Everyone", "Goodbye"};
+//    int w1 = 1;
+//    int w2 = 3;
+//    System.out.println(strings[w1-1]+strings[w2-1]);
+
+//    int[] notstrings = {1, 7, 1, 4, 1, 7, 9, 6, 13, 1, 7};
+//    int target = 7;
+//    for (int i=0;i<notstrings.length;i++)
+//    {
+//        if (notstrings[i]==target)
+//        System.out.println(i);
+//    }
+// 3
+
+//    int[] input = {1, 2, 3, 4, 1, 3, 1, 6, 6, 1, 1};
+//    int isAllLessThan = 100;
+//    boolean huy=true;
+//    for (int i=0;i<input.length;i++){
+//        if (input[i]==isAllLessThan){
+//            huy=false;
+//        break;}
+//        }
+//    System.out.println(huy);
+//}
+
+
+//// false
+    }
+}
