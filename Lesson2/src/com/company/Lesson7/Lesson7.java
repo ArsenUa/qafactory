b4746fdc81a86523d1eb702aca2b26965edc077f
Arsen <mr.landos@gmail.com>
2015-08-08 13:57:11 +0300

    Lesson 2 and 3 home work update

--- Lesson2/src/com/company/Lesson7/Lesson7.java
+++ Lesson2/src/com/company/Lesson7/Lesson7.java
@@ -3,13 +3,13 @@ package com.company.Lesson7;
 /**
  * Created by Arsen on 06.08.15.
  */
-//public class Lesson7 {
-//    public static void main(String[] args) {
-////        int[] b = {1, 10, -20, 30};
-////        for (int a : b) {
-////            System.out.println(a);
-//
-//        }
-//    }
-//
-//}
+public class Lesson7 {
+    public static void main(String[] args) {
+        int[] b = {1, 10, -20, 30};
+        for (int a : b) {
+            System.out.println(a);
+
+        }
+    }
+
+}
